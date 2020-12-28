c41e6753806c03bff81552441a46f78893f910dc
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2020-12-28 21:51:00 +0200

    firebase-file

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLeJOlmv-K9e3IqzbnhnHEL1H2Z82Nzkc",
+  authDomain: "click-up-gsg.firebaseapp.com",
+  projectId: "click-up-gsg",
+  storageBucket: "click-up-gsg.appspot.com",
+  messagingSenderId: "806416301126",
+  appId: "1:806416301126:web:c8011071c458341d5b5e47",
+  measurementId: "G-E9KTKBBFT1",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+export { auth };
