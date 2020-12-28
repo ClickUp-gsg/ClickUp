763409c6402fe38853b7f23b187cfc26c6aaca93
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2020-12-28 21:58:57 +0200

    #2
    sign-text-filed

---
+++ src/components/SignTxtField/style.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin-top: 25px;
+`;
+
+export const Input = styled.div`
+  border: 1px solid
+    ${(props) => (props.error ? "#e54b4b" : "#e9ebf0")};
+  border-radius: 4px;
+  padding: ${(props) => props.padding || "0 12px"};
+  height: 40px;
+  display: flex;
+  align-content: center;
+`;
+
+export const TxtField = styled.input`
+  width: 80%;
+  font-size: 15px;
+  line-height: 22px;
+  &::placeholder {
+    color: #e0e1e2;
+  }
+  /* &[type="password"] {
+    letter-spacing: 0.2em;
+  } */
+`;
+
+export const Icon = styled.div`
+  min-width: 18px;
+  height: 18px;
+  margin-right: 12px;
+  fill: ${(props) => (props.error ? "#e54b4b" : "#b9bec7")};
+  align-self: center;
+  & svg,
+  & svg use {
+    max-width: 100%;
+    max-height: 100%;
+  }
+`;
+
+export const warningIcon = styled.div``;
