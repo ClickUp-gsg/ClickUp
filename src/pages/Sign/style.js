1d22119a4baef50f057aae693c67307b90a02f99
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2020-12-28 22:01:30 +0200

    #2
    sign-page

---
+++ src/pages/Sign/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  background: url("https://app-cdn.clickup.com/login-bg.fcb221668da96b8a1817.svg")
+    bottom/cover no-repeat;
+`;
+
+export const FooterImg = styled.img`
+  max-width: 80px;
+`;
