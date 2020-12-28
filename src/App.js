28ff8bc9aa640e8a0ad6be902b2a00d05d9ec037
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2020-12-28 21:55:03 +0200

    app-router
    theme
    
    #2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import "./App.css";
+import * as S from "./style";
+
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Sign from "./pages/Sign";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  primary: "#7B68EE",
+  primaryDark: "#5f48ea",
+  light: "#d3cdf9",
+  mobileWidth: "520px",
+  ipadWidth: "720px",
+  lapWidth: "1050px",
+};
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <S.App>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/signup">
+              <Sign type="SignUp" />
+            </Route>
+            <Route exact path="/signin">
+              <Sign type="SignIn" />
+            </Route>
+          </Switch>
+        </ThemeProvider>
+      </Router>
+    </S.App>
+  );
 }
 
 export default App;
