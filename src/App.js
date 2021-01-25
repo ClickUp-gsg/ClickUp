14c13110e3a0cba63863ca7beee50eb10cf819e3
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-01-25 23:57:27 +0200

    context
    Provider alias: StateProvider
    
    useContext, alias: useStateValue
    
    #2

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import {
   BrowserRouter as Router,
   Switch,
 } from "react-router-dom";
+import { initState, reducer } from "./components/reducer";
 
 import Home from "./pages/Home";
 import Sign from "./pages/Sign";
+import { StateProvider } from "./components/StateProvider";
 import { ThemeProvider } from "styled-components";
 
 const theme = {
@@ -25,15 +27,17 @@ function App() {
       <Router>
         <ThemeProvider theme={theme}>
           <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/signup">
-              <Sign type="SignUp" />
-            </Route>
-            <Route exact path="/signin">
-              <Sign type="SignIn" />
-            </Route>
+            <StateProvider reducer={reducer} initState={initState}>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/signup">
+                <Sign type="SignUp" />
+              </Route>
+              <Route exact path="/signin">
+                <Sign type="SignIn" />
+              </Route>
+            </StateProvider>
           </Switch>
         </ThemeProvider>
       </Router>
