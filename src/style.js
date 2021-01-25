2dae2304ce734a2b431db504f4b532f373254843
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-01-26 00:26:55 +0200

    hide eye on password input text-filed
    styling when input auto fill
    
    #2

--- src/style.js
+++ src/style.js
@@ -1,3 +1,15 @@
 import styled from "styled-components";
 
-export const App = styled.div``;
+export const App = styled.div`
+  input::-ms-reveal,
+  input::-ms-clear {
+    display: none;
+  }
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus {
+    -webkit-text-fill-color: unset;
+    box-shadow: 0 0 0px 1000px rgb(245, 245, 245) inset;
+    transition: background-color 5000s ease-in-out 0s;
+  }
+`;
