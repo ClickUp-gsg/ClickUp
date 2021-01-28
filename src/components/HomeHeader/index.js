b45aa47f7aeafcad688afe2f1527cfa9a1127563
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-01-28 22:37:31 +0200

    HomeHeader
    #8

---
+++ src/components/HomeHeader/index.js
@@ -0,0 +1,5 @@
+import * as S from "./style";
+
+export default function HomeHeader() {
+  return <S.Container></S.Container>;
+}
---
+++ src/components/HomeHeader/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 17.3%; // 115px
+  width: 100%;
+  background-color: blue;
+`;
