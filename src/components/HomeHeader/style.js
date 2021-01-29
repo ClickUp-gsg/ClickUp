fd9c58eacc1a0fd309ad7e17ed04e6644f503201
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-01-29 20:55:03 +0200

    edit HomeHeader 'not important'
    #8

--- src/components/HomeHeader/style.js
+++ src/components/HomeHeader/style.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 17.3%; // 115px
+  /* height: 17.3%; // 115px */
+  height: 20%;
+  max-height: 115px;
   width: 100%;
   background-color: blue;
 `;
