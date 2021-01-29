e247c3586229ade53c0ce61a894a732afbf8fcea
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-01-29 21:06:08 +0200

    edit assets
    edit homePage
    
    add HomeAside, HomeHeader, HomeMain
    
    #8

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,22 @@
 import * as S from "./style";
+import * as T from "../../components/Typography";
 
 import AddTaskMenu from "../../components/AddTaskMenu";
+import HomeAside from "../../components/HomeAside";
+import HomeHeader from "../../components/HomeHeader";
+import HomeMain from "../../components/HomeMain";
 
 export default function Home() {
   return (
-    <S.Container>
-      <h1>this is home page</h1>
-      <AddTaskMenu />
-    </S.Container>
+    <>
+      <S.Container>
+        <HomeAside />
+        <S.Main>
+          <HomeHeader />
+          <HomeMain />
+        </S.Main>
+        <AddTaskMenu />
+      </S.Container>
+    </>
   );
 }
