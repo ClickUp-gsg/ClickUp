f67d133efbfef9fce415687ccdf5b66355f35709
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-01-29 20:54:48 +0200

    edit HomeAside 'not important'
    #8

--- src/components/HomeAside/style.js
+++ src/components/HomeAside/style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.aside`
   background-color: red;
   height: 100vh;
-  width: 28.5%; //
-  /* width: 300px; */
+  width: 28.5%;
+  max-width: 300px;
   border-right: 1px solid #e9ebf0;
 `;
