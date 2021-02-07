6b4840f4736ab75dcf345f37158a2116313f434d
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-07 22:26:12 +0200

    change LoadingPopUp displayed text to be from props
    #8

--- src/components/LoadingPopUp/index.js
+++ src/components/LoadingPopUp/index.js
@@ -2,13 +2,11 @@ import * as S from "./style";
 import * as T from "../Typography";
 
 import { loadSpinner } from "../../assets";
-import { useStateValue } from "../StateProvider";
 
-export default function LoadingPopUp({ isDisplayed }) {
-  const [{ user }] = useStateValue();
+export default function LoadingPopUp({ isDisplayed, text }) {
   return (
     <S.Container isDisplayed={isDisplayed}>
-      <T.H1>{user.name && `Welcome ${user.name}`}</T.H1>
+      <T.H1>{text}</T.H1>
       <T.Span>{loadSpinner}</T.Span>
     </S.Container>
   );
