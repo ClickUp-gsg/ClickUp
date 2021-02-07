f981d8508cb9840c1be914a775b48ac947ae23bd
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-07 22:26:51 +0200

    give text for LoadingPopUp as props to display when signing
    #8

--- src/pages/Sign/index.js
+++ src/pages/Sign/index.js
@@ -6,14 +6,19 @@ import Footer from "../../components/SignFooter";
 import Header from "../../components/SignHeader";
 import LoadingPopUp from "../../components/LoadingPopUp";
 import { useState } from "react";
+import { useStateValue } from "../../components/StateProvider";
 
 export default function Sign({ type }) {
   const [isLoading, setIsLoading] = useState(false);
+  const [{ user }] = useStateValue();
   return (
     <>
       <S.Container>
         <T.Content padding="0 30px">
-          <LoadingPopUp isDisplayed={isLoading}></LoadingPopUp>
+          <LoadingPopUp
+            isDisplayed={isLoading}
+            text={user.displayName && `Welcome ${user.displayName}`}
+          ></LoadingPopUp>
           <Header type={type} />
           <Card setIsLoading={setIsLoading} type={type} />
           <Footer type={type} />
