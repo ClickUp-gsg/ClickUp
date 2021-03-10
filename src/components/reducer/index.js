f8ce742be3bb48c3faeeb8ed617d0c84781b5710
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-03-10 17:43:17 +0200

    add lists for use reducer state

--- src/components/reducer/index.js
+++ src/components/reducer/index.js
@@ -1,6 +1,13 @@
+import { home, star2 } from "../../assets";
+
 export const initState = {
   tasks: [],
   user: {},
+  lists: [
+    { name: "home", icon: home },
+    { name: "favorites", icon: star2 },
+  ],
+  currentList: "home",
 };
 
 let newTasks;
@@ -30,6 +37,30 @@ export function reducer(state, action) {
         }
       }
       return { ...state, tasks: newTasks };
+    case "TOGGLE_TASK_STAR":
+      newTasks = [...state.tasks];
+      for (let i = 0; i < newTasks.length; i++) {
+        if (newTasks[i].id === action.payload.id) {
+          // let newTaskLists;
+          // let oldTaskLists = [...newTasks[i].lists];
+          // console.log("old lists", oldTaskLists);
+          // const indexOfFavorites = oldTaskLists.indexOf("favorites");
+          // console.log("is has star:", indexOfFavorites);
+          // if (indexOfFavorites === -1) {
+          //   newTaskLists = [...oldTaskLists, "favorites"];
+          // } else {
+          //   oldTaskLists.splice(indexOfFavorites, 1);
+          //   newTaskLists = oldTaskLists;
+          // }
+          // console.log("new lists", newTaskLists);
+          newTasks[i] = {
+            ...newTasks[i],
+            hasStar: !newTasks[i].hasStar,
+            lists: action.payload.newLists,
+          };
+        }
+      }
+      return { ...state, tasks: newTasks };
     case "DELETE_TASK":
       newTasks = [...state.tasks];
       for (let i = 0; i < newTasks.length; i++) {
@@ -39,6 +70,8 @@ export function reducer(state, action) {
         }
       }
       return { ...state, tasks: newTasks };
+    case "EDIT_CURRENT_LIST":
+      return { ...state, currentList: action.payload };
     case "EDIT_USER":
       return { ...state, user: action.payload };
     case "CLEAR_USER":
