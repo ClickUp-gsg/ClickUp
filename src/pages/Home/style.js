3e738829eb37c54ffa7a1d52118879474a296a7f
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-03-10 22:48:26 +0200

    delete test style, change tag of Main to be a div not a main

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -3,9 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  height: 100vh;
 `;
 
-export const Main = styled.main`
+export const Main = styled.div`
   width: 100%;
 `;
