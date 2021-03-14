a1abe8792d61609909638f146cb451aae001d1f9
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-03-14 23:45:35 +0200

    add ability to activate selected list of Tasks

--- src/components/AsideList/index.js
+++ src/components/AsideList/index.js
@@ -1,21 +1,39 @@
-// import * as A from "../../assets";
-
 import * as S from "./style";
 import * as T from "../Typography";
 
+import { useEffect, useRef, useState } from "react";
+
+import { plusSign } from "../../assets";
 import { useStateValue } from "../StateProvider";
 
 export default function AsideList({ isListCollapsed }) {
   const [{ lists, currentList }, dispatch] = useStateValue();
-  function changeActiveList(e, listName) {
+  const [isAddListOpen, setIsAddListOpen] = useState(false);
+  const [newListName, setNewListName] = useState("");
+  const inputAddList = useRef(null);
+  useEffect(() => {
+    if (isListCollapsed) {
+      setIsAddListOpen(false);
+      setNewListName("");
+    }
+  }, [isListCollapsed]);
+  function changeActiveList(listName) {
     dispatch({ type: "EDIT_CURRENT_LIST", payload: listName });
   }
+  function addNewList(e) {
+    e.preventDefault();
+    setIsAddListOpen(false);
+    if (newListName) {
+      setNewListName("");
+    }
+  }
+
   return (
-    <S.ListContainer>
+    <S.ListContainer isCollapsed={isListCollapsed}>
       {lists.map((list) => (
         <S.ItemContainer
           key={list.name}
-          onClick={(e) => changeActiveList(e, list.name)}
+          onClick={(e) => changeActiveList(list.name)}
           isActive={currentList === list.name}
           value={list.name}
         >
@@ -25,6 +43,63 @@ export default function AsideList({ isListCollapsed }) {
           </T.Visibility>
         </S.ItemContainer>
       ))}
+      {!isListCollapsed && (
+        <S.ItemContainer
+          position="absolute"
+          isDisplayed={!isAddListOpen}
+          onClick={() => {
+            !isListCollapsed && setIsAddListOpen(true);
+            inputAddList.current.focus();
+          }}
+        >
+          <T.Flex width="100%" justify="center" margin="10px">
+            <T.SvgContainer
+              color="#898d93"
+              hover_color="#898d93"
+              fill="true"
+            >
+              {plusSign}
+            </T.SvgContainer>
+          </T.Flex>
+        </S.ItemContainer>
+      )}
+      <S.AddListInputContainer isDisplayed={isAddListOpen}>
+        <form onSubmit={addNewList}>
+          <T.Flex>
+            <T.Input
+              ref={inputAddList}
+              value={newListName}
+              onChange={(e) => setNewListName(e.target.value)}
+              width="100%"
+              height="32px"
+              color="black"
+              padding="0 7px"
+              placeholder="List Name"
+              radius="4px 0 0 4px"
+            />
+            <T.Button
+              padding="0 10px"
+              radius="0 4px 4px 0"
+              height="32px"
+              background="black"
+              type="light"
+            >
+              {newListName ? (
+                "Add"
+              ) : (
+                <T.SvgContainer
+                  color="#898d93"
+                  width="17px"
+                  fill="true"
+                  rotate="45deg"
+                >
+                  {plusSign}
+                </T.SvgContainer>
+              )}
+            </T.Button>
+          </T.Flex>
+        </form>
+      </S.AddListInputContainer>{" "}
     </S.ListContainer>
   );
 }
