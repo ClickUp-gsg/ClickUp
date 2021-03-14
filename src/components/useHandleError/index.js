2234d32674ad4c1e911ec514ab7c8ba66f17eb72
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-03-14 23:55:30 +0200

    replace normal chaning with optional chaining

--- src/components/useHandleError/index.js
+++ src/components/useHandleError/index.js
@@ -1,7 +1,7 @@
 export default function handleError(e, errorsKeywords) {
   if (e.errorThrower !== "front-end") {
     errorsKeywords.some((filedName) => {
-      if (e.code.includes(filedName)) {
+      if (e?.code?.includes?.(filedName)) {
         e.code = filedName;
         return true;
       } else if (e.code.includes("network")) {
