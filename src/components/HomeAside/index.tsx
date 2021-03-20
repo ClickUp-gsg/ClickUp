a3de6b1568b35b86ec02f10a65c5a3320258abe9
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-03-20 10:30:17 +0200

    HomeAdide from  js to tsx

--- src/components/HomeAside/index.tsx
+++ src/components/HomeAside/index.tsx
@@ -2,7 +2,7 @@ import * as A from "../../assets";
 import * as S from "./style";
 import * as T from "../Typography";
 
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 import List from "../AsideList";
 import { auth } from "../../firebase";
@@ -10,11 +10,52 @@ import { clearDB } from "../useIndexedDB";
 import { useHistory } from "react-router-dom";
 import { useStateValue } from "../StateProvider";
 
+interface listItems {
+  current?: HTMLButtonElement[];
+  [key: string]: any;
+}
+
+interface list {
+  current?: HTMLElement;
+  scrollTop?: number;
+}
+
 export default function HomeAside() {
-  const [{ user }, dispatch] = useStateValue();
+  const [{ user, lists }, dispatch] = useStateValue();
   const history = useHistory();
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [searchValue, setSearchValue] = useState("");
+  const list = useRef<list>();
+  const listItemsRefsArr = useRef<listItems>({});
+
+  useEffect(() => {
+    if (!isCollapsed) {
+      setSearchValue("");
+    }
+  }, [isCollapsed]);
+
+  function changeSearchValue(e: ChangeEvent<HTMLInputElement>) {
+    const { value } = e.target;
+    setSearchValue(value);
+    let isFirstSearchResultFounded = false;
+    lists.forEach((listItem: { name: string }) => {
+      const resultElement = listItemsRefsArr.current[listItem.name];
+      if (
+        value &&
+        value.match(/^ *$/) === null &&
+        listItem.name.includes(value.trim()) &&
+        !isFirstSearchResultFounded
+      ) {
+        isFirstSearchResultFounded = true;
+        console.log(resultElement.firstChild);
+        resultElement.lastChild.style.backgroundColor = "#abff0070";
+        list!.current!.scrollTop = resultElement.offsetTop;
+      } else {
+        resultElement.lastChild.style.backgroundColor = "unset";
+      }
+    });
+  }
+
   function logout() {
     if (user?.uid) {
       clearDB("user");
@@ -23,11 +64,7 @@ export default function HomeAside() {
     }
     history.push("/signin");
   }
-  useEffect(() => {
-    if (!isCollapsed) {
-      setSearchValue("");
-    }
-  }, [isCollapsed]);
+
   return (
     <S.Container isCollapsed={isCollapsed}>
       <S.LogoSection>
@@ -45,21 +82,28 @@ export default function HomeAside() {
       <S.SearchBox
         setIsListCollapsed={setIsCollapsed}
         isListCollapsed={isCollapsed}
-        setSearchValue={setSearchValue}
+        setSearchValue={changeSearchValue}
         searchValue={searchValue}
       />
-      <List isListCollapsed={isCollapsed}></List>
+      <List
+        ItemsRefsArr={listItemsRefsArr}
+        listRef={list}
+        isListCollapsed={isCollapsed}
+      ></List>
       <S.Footer>
         <T.Flex
           width="100%"
           justify={user?.name ? "space-between" : "flex-end"}
         >
-          <T.Flex>
-            {user?.photo || <S.CurrentUserImg src={user?.photoURL} />}
+          <T.Flex display={isCollapsed ? "none" : ""}>
+            {user?.photoURL && (
+              <S.CurrentUserImg src={user?.photoURL} />
+            )}
             <T.P margin="0 0 0 4px">{user.name}</T.P>
           </T.Flex>
           <S.LogoutBtn
             onClick={logout}
+            isDisplayed={!isCollapsed}
             type="light"
             height="40px"
             padding="0 15px"
@@ -75,12 +119,11 @@ export default function HomeAside() {
               >
                 {A.logoutDoor}
               </T.SvgContainer>
-              <T.P color="black">
+              <T.P size={isCollapsed ? "0" : ""} color="black">
                 {user?.uid ? "Logout" : "login"}
               </T.P>
             </T.Flex>
           </S.LogoutBtn>
-          {/* </T.Link> */}
         </T.Flex>
       </S.Footer>
     </S.Container>
