5041cbcb4cb610e8cc593064795829234f06fc28
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-03-20 10:25:58 +0200

    reStyle AsideList to have scroll when overflow

--- src/components/AsideList/style.js
+++ src/components/AsideList/style.js
@@ -1,22 +1,43 @@
 import styled from "styled-components";
 
 export const ListContainer = styled.div`
-  margin-top: 20px;
   position: relative;
+  margin-bottom: 60px;
+  padding-bottom: 8px;
+  margin-top: 10px;
+  scroll-behavior: smooth;
+  overflow-y: ${(props) => (props.isCollapsed ? "hidden" : "auto")};
+  overflow-x: hidden;
   border-top: ${(props) =>
     props.isCollapsed
       ? "1px solid #e9ebf0"
       : "1px solid transparent"};
+  /* Scrollbar style */
+  &::-webkit-scrollbar-thumb {
+    border-color: #eee;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #babac0;
+    border-radius: 12px;
+    border: 2px solid #fafbfc;
+  }
+  &::-webkit-scrollbar-track {
+    background: #eee;
+  }
+  &::-webkit-scrollbar {
+    width: 10px;
+    border: 1px solid black;
+  }
 `;
 
 export const ItemContainer = styled.button`
   display: flex;
   align-items: center;
-  height: 47px;
+  height: 49.5px;
   color: #7c828d;
   width: 100%;
   background-color: white;
-  padding: 10px 20px 10px 15px;
+  padding: 10px 13px 10px 15px;
   cursor: pointer;
   z-index: 1;
   transition: 0.2s;
@@ -30,18 +51,13 @@ export const ItemContainer = styled.button`
   }
 `;
 
-export const Icon = styled.i`
-  fill: #7c828d;
-  display: inline-block;
-  margin-right: 10px;
-  min-width: 18px;
-`;
-
 export const AddListInputContainer = styled.div`
-  padding: 12px 20px 12px 15px;
+  padding: 12px 15px 12px 15px;
   opacity: ${(props) => !props.isDisplayed && "0"};
   z-index: ${(props) => (!props.isDisplayed ? "-1" : "9")};
-  width: 100%;
+  width: 98%;
+  margin: 0 2px;
+  border-radius: 28px;
   height: 57px;
   transition: 0.2s;
   background-color: ${(props) => props.theme.primary};
