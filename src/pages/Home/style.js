ef1a9ea68d48a28bf300f679d0962fc3e7f6c948
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-03-30 18:50:04 +0300

    mobile & ipad responsive Home

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -3,8 +3,15 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   width: 100%;
+  @media only screen and (max-width: 600px) {
+    display: block;
+  }
 `;
 
 export const Main = styled.div`
   width: 100%;
+  @media only screen and (max-width: 855px) {
+    margin-left: 50px;
+    width: calc(100% - 50px);
+  }
 `;
