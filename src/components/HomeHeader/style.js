f475912058382d25c856e43e62bc246e89477b0b
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-03-30 18:52:45 +0300

    small  HomeHeader in mobile

--- src/components/HomeHeader/style.js
+++ src/components/HomeHeader/style.js
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const Container = styled.header`
   width: 100%;
   height: 115px;
+  @media only screen and (max-width: 600px) {
+    height: 45px;
+  }
 `;
